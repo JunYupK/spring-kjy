c55afa5c9a38a5945bc5be253922a91d4f5ded98
yongbul <top1506@naver.com>
2024-02-26 20:04:37 +0900

    init project

---
+++ src/main/java/com/kjy/SpringKjyApplication.java
@@ -0,0 +1,13 @@
+package com.kjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKjyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKjyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjy/SpringKjyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKjyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
